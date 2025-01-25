df60b3b7ee08073177fab2866027b461c4672f90
axin <chenlixin0824@163.com>
2025-01-25 14:39:53 +0800

    edit:修改日志打印格式

--- config/zap.go
+++ config/zap.go
@@ -4,56 +4,43 @@ import (
 	"fmt"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
-	"os"
 	"time"
 )
 
 // InitLogger 初始化Logger
-func InitLogger() {
-	// 设置日志输出格式
+func InitLogger() *zap.Logger {
+	// 创建自定义的 encoder 配置
 	encoderConfig := zapcore.EncoderConfig{
-		TimeKey:        "time",
-		LevelKey:       "level",
-		NameKey:        "logger",
-		CallerKey:      "caller",
-		MessageKey:     "msg",
-		StacktraceKey:  "stacktrace",
+		TimeKey:        "", // 时间我们在日志消息中自己格式化
+		LevelKey:       "L",
+		NameKey:        "N",
+		CallerKey:      "C",
+		MessageKey:     "M",
+		StacktraceKey:  "S",
 		LineEnding:     zapcore.DefaultLineEnding,
 		EncodeLevel:    zapcore.CapitalLevelEncoder,
-		EncodeTime:     timeEncoder,
-		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.StringDurationEncoder,
 		EncodeCaller:   zapcore.ShortCallerEncoder,
 	}
 
-	// 创建日志文件
-	now := time.Now()
-	logFileName := fmt.Sprintf("logs/%s.log", now.Format("2006-01-02"))
-
-	// 确保日志目录存在
-	if err := os.MkdirAll("logs", 0777); err != nil {
-		panic(fmt.Sprintf("创建日志目录失败: %v", err))
+	// 使用 console encoder 而不是 json encoder
+	config := zap.Config{
+		Level:            zap.NewAtomicLevelAt(zap.InfoLevel),
+		Development:      true,
+		Encoding:         "console", // 使用 console 编码器
+		EncoderConfig:    encoderConfig,
+		OutputPaths:      []string{"stdout"},
+		ErrorOutputPaths: []string{"stderr"},
 	}
 
-	// 打开日志文件
-	logFile, _ := os.OpenFile(logFileName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	logger, err := config.Build()
+	if err != nil {
+		panic(fmt.Sprintf("日志初始化失败: %v", err))
+	}
 
-	// 设置日志级别
-	atomicLevel := zap.NewAtomicLevel()
-	atomicLevel.SetLevel(zap.InfoLevel)
-
-	core := zapcore.NewCore(
-		zapcore.NewJSONEncoder(encoderConfig),
-		zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout), zapcore.AddSync(logFile)),
-		atomicLevel,
-	)
-
-	// 开启开发模式，堆栈跟踪
-	caller := zap.AddCaller()
-	development := zap.Development()
-
-	// 构造日志
-	MGA_LOG = zap.New(core, caller, development)
-	defer MGA_LOG.Sync()
+	MGA_LOG = logger
+	return logger
 }
 
 // timeEncoder 自定义时间编码器
--- middleware/logMiddleware.go
+++ middleware/logMiddleware.go
@@ -1,35 +1,121 @@
 package middleware
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"go.uber.org/zap"
-	"strconv"
+	"strings"
 	"time"
 )
+
+type responseBodyWriter struct {
+	gin.ResponseWriter
+	body *bytes.Buffer
+}
+
+func (w responseBodyWriter) Write(b []byte) (int, error) {
+	w.body.Write(b)
+	return w.ResponseWriter.Write(b)
+}
 
 // 日志中间件
 func LoggerMiddleware(logger *zap.Logger) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		// 生成并设置 traceId
+		traceId := uuid.New().String()
+		c.Set("traceId", traceId)
+
 		// 记录请求开始的时间
 		startTime := time.Now()
+
+		// 获取请求参数
+		var requestParams string
+		method := c.Request.Method
+		if method == "GET" {
+			requestParams = c.Request.URL.RawQuery
+		} else {
+			rawData, _ := c.GetRawData()
+			if len(rawData) > 0 {
+				requestParams = string(rawData)
+			}
+			// 重新设置请求体，因为GetRawData会清空body
+			c.Request.Body = NewBuffer(rawData)
+		}
+
+		// 获取请求头
+		headers := make(map[string]string)
+		for k, v := range c.Request.Header {
+			headers[k] = strings.Join(v, ",")
+		}
+		headerJSON, _ := json.Marshal(headers)
+
+		// 包装 ResponseWriter 以捕获响应内容
+		responseBody := &bytes.Buffer{}
+		writer := responseBodyWriter{
+			ResponseWriter: c.Writer,
+			body:           responseBody,
+		}
+		c.Writer = writer
 
 		// 继续执行后续的中间件和路由处理
 		c.Next()
 
 		// 记录请求的相关信息
-		duration := time.Since(startTime).Milliseconds()
+		duration := time.Since(startTime)
 		statusCode := c.Writer.Status()
-		method := c.Request.Method
 		path := c.Request.URL.Path
 		clientIP := c.ClientIP()
 
-		// 构建日志信息
-		logger.Info("Request",
-			zap.String("method", method),
-			zap.String("path", path),
-			zap.String("client_ip", clientIP),
-			zap.Int("status_code", statusCode),
-			zap.String("duration", strconv.FormatInt(duration, 10)+"ms"),
-		)
+		// 获取响应内容
+		responseContent := responseBody.String()
+		// 如果响应内容太长，可以截断
+		if len(responseContent) > 1000 {
+			responseContent = responseContent[:1000] + "..."
+		}
+
+		// 构建自定义格式的日志信息
+		timeStr := time.Now().Format("2006-01-02 15:04:05")
+		logMessages := []string{
+			fmt.Sprintf("[%s] TraceID=%s", timeStr, traceId),
+			fmt.Sprintf("URL: %s", path),
+			fmt.Sprintf("Method: %s", method),
+			fmt.Sprintf("Params: %s", requestParams),
+			fmt.Sprintf("Headers: %s", string(headerJSON)),
+			fmt.Sprintf("ClientIP: %s", clientIP),
+			fmt.Sprintf("Status: %d", statusCode),
+			fmt.Sprintf("Duration: %s", duration.String()),
+			fmt.Sprintf("Response: %s", responseContent),
+			"----------------------------------------",
+		}
+
+		logMessage := strings.Join(logMessages, "\n")
+
+		// 根据状态码选择日志级别
+		if statusCode >= 400 {
+			logger.Error(logMessage)
+		} else {
+			logger.Info(logMessage)
+		}
 	}
+}
+
+// NewBuffer 创建新的buffer用于重新设置请求体
+func NewBuffer(data []byte) *Buffer {
+	return &Buffer{data: data}
+}
+
+type Buffer struct {
+	data []byte
+}
+
+func (b *Buffer) Read(p []byte) (n int, err error) {
+	copy(p, b.data)
+	return len(b.data), nil
+}
+
+func (b *Buffer) Close() error {
+	return nil
 }
